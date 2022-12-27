05be8c54364cbf13a90b85fb93eb1aba2a5241f0
Shaun <shaun@shaunpud.com>
2022-12-27 18:34:14 +0800

    Create install-utils.py

---
+++ install-utils.py
@@ -0,0 +1,27 @@
+from pyinfra.operations import apt
+
+apt.packages(
+    _sudo=True,
+    name="Installing utilities",
+    packages=[
+        "curl",
+        "git",
+        "htop",
+        "iptables",
+        "lftp",
+        "lsb-release",
+        "lsof",
+        "pigz",
+        "pwgen",
+        "rsync",
+        "screen",
+        "sudo",
+        "telnet",
+        "unzip",
+        "wget",
+        "whois",
+        "zip",
+    ],
+    latest=True,
+    update=True,
+)
