dc759553d0c1cd50d5274e43c76d31505bd86af1
shaunpud <shaun@shaunpud.com>
2023-05-05 16:45:27 +0800

    pipx

--- install-python.py
+++ install-python.py
@@ -1,22 +1,15 @@
-from pyinfra.operations import apt, server
+from pyinfra.operations import apt
 
 apt.packages(
     _sudo=True,
     name="Installing python",
     packages=[
         "build-essential",
+        "pipx",
         "python3-dev",
         "python3-pip",
         "python3-venv",
     ],
     latest=True,
     update=True,
-)
-
-server.shell(
-    _sudo=True,
-    name="Updating pip",
-    commands=[
-        "pip3 install --upgrade pip",
-    ],
 )
