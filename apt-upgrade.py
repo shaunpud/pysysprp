3591cf42563815c8b28298ba0f829a23b232dfeb
shaunpud <shaun@shaunpud.com>
2023-12-14 19:57:18 +0800

    reboot

--- apt-upgrade.py
+++ apt-upgrade.py
@@ -1,4 +1,8 @@
-from pyinfra.operations import apt
+import sys
+
+from pyinfra.operations import apt, server
+
+reboot = input("\nReboot? [y/N] ")
 
 apt.update(
     _sudo=True,
@@ -9,3 +13,9 @@ apt.dist_upgrade(
     _sudo=True,
     name="Upgrading packages",
 )
+
+if reboot.lower().strip() == "y":
+    server.reboot(
+        _sudo=True,
+        name="Rebooting",
+    )
