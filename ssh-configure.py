73bec5fa0bc95b0b6ac3591eaa091f66873160eb
shaunpud <shaun@shaunpud.com>
2024-02-15 09:56:28 +0800

    hardening

--- ssh-configure.py
+++ ssh-configure.py
@@ -38,6 +38,19 @@ server.shell(
     },
 )
 
+files.put(
+    _sudo=True,
+    name="Hardening as per sshaudit.com",
+    src=StringIO(
+        f"""KexAlgorithms sntrup761x25519-sha512@openssh.com,curve25519-sha256,curve25519-sha256@libssh.org,gss-curve25519-sha256-,diffie-hellman-group16-sha512,gss-group16-sha512-,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256
+Ciphers aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com
+HostKeyAlgorithms ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,sk-ssh-ed25519@openssh.com,sk-ssh-ed25519-cert-v01@openssh.com,rsa-sha2-512,rsa-sha2-512-cert-v01@openssh.com,rsa-sha2-256,rsa-sha2-256-cert-v01@openssh.com"""
+    ),
+    dest="/etc/ssh/sshd_config.d/98-hardening.conf",
+    mode=600,
+)
+
 server.service(
     _sudo=True,
     name="Reloading ssh service",
