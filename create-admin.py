e13c831da50d1c8b87ee19a64c0fb090b30fa7a6
shaunpud <shaun@shaunpud.com>
2024-05-16 10:12:30 +0800

    ua

--- create-admin.py
+++ create-admin.py
@@ -11,7 +11,7 @@ administrator = input("\nAdministrator: ")
 if not administrator or not re.match(r"^[a-z0-9-_]+$", administrator, flags=re.I):
     exit("Invalid Username")
 
-agentsdata = "https://raw.githubusercontent.com/jnrbsn/user-agents/main/user-agents.json"
+agentsdata = "https://raw.githubusercontent.com/EIGHTFINITE/top-user-agents/main/index.json"
 useragents = json.loads(urlopen(agentsdata).read().decode())
 
 apt.packages(
